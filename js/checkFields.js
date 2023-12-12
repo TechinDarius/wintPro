19e1c237dc352213e3466bd7c92c5e94c65f186b
Darius <darius.mlecka@stud.techin.lt>
2023-12-12 14:45:28 +0200

    changing js

--- js/checkFields.js
+++ js/checkFields.js
@@ -1,20 +1,3 @@
-// function testRegexp(text) {
-//     // Regexp
-//     const regexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-//     return regexp.test(text);
-// }
-
-// function checkField() {
-//     let userInput = document.getElementById('input__contacts__button').value;
-//     let result = testRegexp(userInput);
-
-//     if (result) {
-//         alert('El pastas yra teisingas');
-//     } else {
-//         alert('El pastas yra neteisingas');
-//     }
-// }
-//////
 
 function testRegexp(text) {
     const regexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
